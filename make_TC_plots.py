c7237db4f2e5bf500cd363e7f64505c4121b712a
Jordan Ashley <jashley6@vols.utk.edu>
2025-07-17 20:39:09 -0700

    :fire::beers:

--- make_TC_plots.py
+++ make_TC_plots.py
@@ -5,10 +5,8 @@ import json
 import argparse
 from PIL import Image
 #Import HBI plotting scripts
-import SD
 import RC
 import NO
-import OCS
 from common_functions import *
 import ITkPDB_matters as db
 
@@ -20,7 +18,7 @@ parser.add_argument("-d", "--HBI_directory",
 parser.add_argument("-db", "--database",
   help="Queries ATLAS ITk Production Database, instead of local files.", action='store_true')
 parser.add_argument("-t", "--tests",
-  help="Test types to be plotted (SD, 3PG, 10PG, NO, OCS). If not specified, all will be plotted.", nargs="+", default = ["SD", "3PG", "10PG", "NO", "OCS"])
+  help="Test types to be plotted (3PG, 10PG, NO). If not specified, all will be plotted.", nargs="+", default = ["3PG", "10PG", "NO"])
 parser.add_argument("-n", "--noise_only", help="When making the 3PG/10PG plots, only make plots for the noise, not the gain or VT50", action='store_true')
 args = parser.parse_args()
 
@@ -36,35 +34,20 @@ if not query_db: #if using local files
 
     #Get files
     files = fetch_files(HBI_directory)
-    SD_files   = [f'{HBI_directory}/{SD_file}' for SD_file in files["SD"]]
     TPG_files  = [f'{HBI_directory}/{TPG_file}' for TPG_file in files["TPG"]]
     RC_files   = [f'{HBI_directory}/{RC_file}' for RC_file in files["RC"]]
     NO_files   = [f'{HBI_directory}/{NO_file}' for NO_file in files["NO"]]
-    OCS_files  = [f'{HBI_directory}/{OCS_file}' for OCS_file in files["OCS"]]
 
 if query_db: #if getting data from the database
 
-    SD_files, TPG_files, RC_files, NO_files, OCS_files = db.get_files()
-    files = {'SD': SD_files,
-             '3PG': TPG_files,
+    TPG_files, RC_files, NO_files = db.get_files()
+    files = {'3PG': TPG_files,
              '10PG': RC_files,
-             'NO': NO_files,
-             'OCS': OCS_files} #files sorted by type
+             'NO': NO_files} #files sorted by type
 
-SD_plots    = []
 TPG_plots   = []
 RC_plots    = []
 NO_plots    = []
-OCS_plots   = []
-
-#Make SD plots
-if "SD" in test_types:
-    print("\nMaking Strobe Delay plots...")
-
-    for SD_file in SD_files:
-        SD_data = retrieve_data(SD_file)
-        SD_plots.append(SD.make_plots(SD_data))
-    print(f"\n{GREEN}Strobe Delay plots complete!{RESET}")
 
 #Make 3PG plots
 if "3PG" in test_types:
@@ -93,15 +76,6 @@ if "NO" in test_types:
         NO_plots.append(NO.make_plots(NO_data))
     print(f"\n{GREEN}Noise Occupancy plots complete!{RESET}")
 
-#Make OCS plots
-if "OCS" in test_types:
-    print("\nMaking Open Channel Search plots...")
-
-    for OCS_file in OCS_files:
-        OCS_data = retrieve_data(OCS_file)
-        OCS_plots.append(OCS.make_plots(OCS_data))
-    print(f"\n{GREEN}Open Channel Search plots complete!{RESET}")
-
 #Make HBI summary plots
 print("\nMaking Thermal Cycling summary plots...")
 
@@ -121,7 +95,7 @@ print(f"\n{GREEN}Thermal Cycling summary plots complete!{RESET}")
 
 #Make single PDF from all made plots
 print("\nMaking PDF...")
-all_plots0 = SD_plots + make_one_list(TPG_plots) + make_one_list(RC_plots) + NO_plots + OCS_plots #all plots made
+all_plots0 = make_one_list(TPG_plots) + make_one_list(RC_plots) + NO_plots #all plots made
 all_plots  = (make_one_list(all_plots0)) #reformat
 component  = get_component(RC_data) #module serial number
 date       = RC_data["date"][:10] #date that HBI was run
